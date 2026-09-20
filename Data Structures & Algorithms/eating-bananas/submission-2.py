734ef9f5d14b7d9d6e48e3c8a534941a0f1a8917
Munteanu David-Ioan <119242980+davidmunteanu@users.noreply.github.com>
2026-09-20 12:45:24 +0300

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left, right = 1, 0
+        candidateK = sys.maxsize
+        for pile in piles:
+            right = max(right, pile)
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            trialH = 0
+            for pile in piles:
+                trialH += (pile + mid - 1) // mid
+
+            if trialH > h:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return left
